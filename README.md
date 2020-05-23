0ec0bddda351911dc24cf09a80bac094772cc4f3
jsgm <34169065+jsgm@users.noreply.github.com>
2020-05-23 20:32:55 +0200

    README.md update, fixed constants names fatal error.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![License](https://img.shields.io/github/license/jsgm/wp-hardener)
 
+!!! This plugin is not finished yet but it's fine to use in the current version in a production enviroment.
+
 # wp-hardener
 wp-hardener is a ready to use plugin which adds an extra layer of security and performance improvements to your WordPress. Most of the features listed here are a recopillation from functions that I've used in different WordPress sites. 
 
@@ -7,7 +9,7 @@ wp-hardener is a ready to use plugin which adds an extra layer of security and p
 * WP 5.4.1 or higher.
 * SSL Certificate enabled and installed.
 
-# Features
+# Disabling options
 In case you need a specific feature not being disabled, modify the ***plugin.php*** file before install. At firsts lines you'll find some constants like shown here. Set to FALSE.
 
 ```php
@@ -16,8 +18,8 @@ define("DISABLE_OEMBED", FALSE);
 
 # Security features
 - Forces SSL for /wp-admin.
-- Disables file editor.
-- Removes file versions if WP_DEBUG is set to FALSE.
+- Disables the built-in file editor.
+- Removes files versions from URLs if WP_DEBUG is set to FALSE.
 - Fully disables XMLRPC.
 - Removes the WLW meta tag and file (wlwmanifest.xml) for Windows Live Writer.
 - Removes the license files and readme.html from root folder.
@@ -25,12 +27,15 @@ define("DISABLE_OEMBED", FALSE);
 - Removes meta tag from Visual Composer / WP Bakery.
 - Adds security headers. You can checkout yours on [securityheaders.com](https://securityheaders.com/)
 - Disables oEmbed.
-- Removes unwanted headers.
+- Removes Link HTTP header.
+- Disables WordPress URL guessing.
+- Will send a 404 response in wp-login.php if the User-Agent is not legit. This will add a little help to stop brute-force attacks altought it's easy to bypass.
 
 # Performance features
 - Disables [wptexturize](https://developer.wordpress.org/reference/functions/wptexturize/).
 - Limit [posts revisions](https://kinsta.com/knowledgebase/wordpress-revisions/) to 3. 
-- Disables [emojis](https://kinsta.com/knowledgebase/disable-emojis-wordpress/). 
+- Disables [emojis](https://kinsta.com/knowledgebase/disable-emojis-wordpress/).
+- Switchs local jQuery files to Google jQuery CDN.
 
 # Tested on
 | WP Version | Working |
--- plugin.php
+++ plugin.php
@@ -7,6 +7,9 @@ Author: José Aguilera
 Version: 0.1
 Author URI: https://github.com/jsgm
 */
+use Jaybizzle\CrawlerDetect\CrawlerDetect;
+
+require "inc/admin.php";
 
 if(!defined("ABSPATH")){
     header('HTTP/1.0 403 Forbidden');
@@ -25,6 +28,10 @@ if(!defined("PHP_INT_MIN")){
 if(!defined("FORCE_SSL_ADMIN")){
     // Forces SSL on admin panel.
     define("FORCE_SSL_ADMIN", TRUE);
+
+    if(isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && strpos($_SERVER["HTTP_X_FORWARDED_PROTO"], "https") !== FALSE){
+        $_SERVER['HTTPS']='on';
+    }
 }
 
 if(!defined('DISALLOW_FILE_EDIT')){
@@ -43,7 +50,7 @@ if(!defined("WP_POST_REVISIONS")){
 // Configuration
 define("ADD_SECURITY_HEADERS", TRUE);
 define("HIDE_WP_VERSION", TRUE);
-define("VERIFY_CHECKSUMS", TRUE);
+define("VERIFY_CHECKSUMS", FALSE);
 define("DISABLE_WLWMANIFEST", TRUE);
 define("DISABLE_OEMBED", TRUE);
 define("DISABLE_LICENSE_FILES", TRUE);
@@ -54,9 +61,18 @@ define("REMOVE_SHORTLINKS", TRUE);
 define("REMOVE_UNWANTED_HEADERS", TRUE);
 define("REMOVE_WP_BAKERY_GENERATOR", TRUE);
 define("REMOVE_FILES_VERSIONS", TRUE);
+define("REMOVE_LICENSE_FILES", TRUE);
+define("BLOCK_CRAWLERS_ON_LOGIN", TRUE);
+define("CHANGE_LOGIN_URL", TRUE);
+define("NEW_LOGIN_URL", "url");
+define("DISABLE_URL_GUESSING", TRUE);
+define("REPLACE_JQUERY_WITH_GOOGLE_CDN", TRUE);
 
 class wphardener{
     public function __construct(){
+        if(REPLACE_JQUERY_WITH_GOOGLE_CDN){
+            //
+        }
         if(ADD_SECURITY_HEADERS){
             $this->security_headers();
         }
@@ -66,7 +82,7 @@ class wphardener{
         if(REMOVE_LICENSE_FILES){
             $this->remove_license_files();
         }
-        if(DISALBE_XMLRPC){
+        if(DISABLE_XMLRPC){
             $this->disable_xmlrpc();
         }
         if(DISABLE_WLWMANIFEST){
@@ -96,13 +112,38 @@ class wphardener{
         if(DISABLE_EMOJIS){
             $this->disable_emojis();
         }
+        if(BLOCK_CRAWLERS_ON_LOGIN){
+            $this->block_crawlers_on_login();
+        }
+        if(DISABLE_URL_GUESSING){
+            $this->disable_url_guessing();
+        }
+    }
+
+    private function is_admin_login(){
+        return ($GLOBALS['pagenow'] === 'wp-login.php' ? true : false);
+    }
+    private function block_crawlers_on_login(){
+        // Block crawlers in login page.
+        add_action("init", function(){
+            if($this->is_admin_login()){
+                require dirname(__FILE__)."/vendor/autoload.php";
+                $CrawlerDetect = new CrawlerDetect;
+                if($CrawlerDetect->isCrawler()){
+                    header('HTTP/1.0 404 Not Found', true, 404);
+                    die();
+                }
+            }
+        }, PHP_INT_MAX);
     }
 
     private function disable_wptexturize(){
+        // Disable wptexturize.
         add_filter( 'xmlrpc_enabled', '__return_false' );
     }
     
     private function clear_version_from_string($s){
+        // Remove version from URL.
         if(is_string($s) && strlen($s)>0 && strpos($s, 'ver=') !== FALSE){
             $s = remove_query_arg('ver', $s);
         }
@@ -149,12 +190,7 @@ class wphardener{
     }
 
     private function security_headers(){
-        /*
-         * Adds security headers. This might not work using WP-Rocket or other cache plugins.
-         * 
-         * More info: https://securityheaders.com/
-         * 
-         */
+        // Adds security headers. This might not work using WP-Rocket or other cache plugins. Check out: https://securityheaders.com/
         add_action('send_headers', function(){
             header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
             header("X-XSS-Protection: 1; mode=block");
@@ -167,11 +203,9 @@ class wphardener{
             header("Referrer-Policy: strict-origin-when-cross-origin");
         });
     }
+
     private function remove_bakery_generator(){
-        /*
-         * Removes meta data for WP Bakery Page Builder.
-         * 
-         */
+        // Removes meta data for WP Bakery Page Builder.
         add_action('wp_head', function(){
             if(class_exists('Vc_Base') && function_exists("visual_composer")) {
                 remove_action('wp_head', array(visual_composer(), 'addMetaData'));
@@ -180,18 +214,12 @@ class wphardener{
     }
 
     private function remove_headers(){
-        /*
-         * Removes unnecesary headers.
-         * 
-         */
+        // Removes unnecesary headers.
         remove_action('template_redirect', 'rest_output_link_header', 11, 0);
     }
 
     private function remove_license_files(){
-        /*
-         * Deletes license files and readme.
-         * 
-         */
+        // Deletes license files and readme.
         try{
             $files = ["license.txt", "licencia.txt", "readme.html"];
             foreach($files as $file){
@@ -206,16 +234,9 @@ class wphardener{
     }
 
     private function disable_xmlrpc(){
-        /*
-         * Fully disables XMLRPC.
-         * 
-         */
-        // Remove text-link from head.
-        remove_action("wp_head", "rsd_link");
-
-        // Disable XMLRPC.
-        add_filter("xmlrpc_enabled", "__return_false");
-
+        // Fully disables XMLRPC.
+        remove_action("wp_head", "rsd_link"); // Remove text-link from head.
+        add_filter("xmlrpc_enabled", "__return_false"); // Disable XMLRPC.
         // Disable endpoints.
         add_filter("xmlrpc_methods", function(){
             return [];
@@ -230,8 +251,8 @@ class wphardener{
             header_remove('X-Pingback');
         }, 1000);
 
-        // Make /xmlrpc.php return 403.
         add_action("init", function(){
+            // Make /xmlrpc.php return 403.
             if(defined("XMLRPC_REQUEST")){
                 header('HTTP/1.0 403 Forbidden');
                 header("Content-type:text/plain; charset=utf-8");
@@ -256,6 +277,16 @@ class wphardener{
 
     }
 
+    private function disable_url_guessing(){
+        // By Andrew Nacin: https://profiles.wordpress.org/nacin/
+        add_filter('redirect_canonical', function($redirect_url){
+            if(is_404()){
+                return false;
+            }
+            return $redirect_url;
+        });
+    }
+
     private function remove_shortlinks(){
         add_filter('after_setup_theme', function(){
             // Remove HTML meta tag.
@@ -264,7 +295,6 @@ class wphardener{
             // Remove HTTP header.
             remove_action( 'template_redirect', 'wp_shortlink_header', 11);
         });
-        add_filter('after_setup_theme', 'remove_redundant_shortlink');
     }
 
     private function disable_oembed(){
@@ -282,8 +312,8 @@ class wphardener{
     }
 
     private function verify_checksums(){
-        if ( defined( 'ABSPATH' ) ) {
-            include( ABSPATH . 'wp-includes/version.php' );
+        if(defined('ABSPATH')){
+            include(ABSPATH.'wp-includes/version.php');
             $wp_locale = isset( $wp_local_package ) ? $wp_local_package : 'en_US';
             $apiurl = 'https://api.wordpress.org/core/checksums/1.0/?version=' . $wp_version . '&locale=' .  $wp_locale;
             $json = json_decode ( file_get_contents ( $apiurl ) );
@@ -300,9 +330,34 @@ class wphardener{
             }
         }
     }
+    
+    private function check_http2(){
+       /* $url = "https://google.com";
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL            => $url,
+            CURLOPT_HEADER         => true,
+            CURLOPT_NOBODY         => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_2_0,
+        ]);
+        $response = curl_exec($ch);        
+        if ($response !== false && strpos($response, "HTTP/2.0") === 0) {
+            echo "Server of the URL has HTTP/2.0 support.";
+        } elseif ($response !== false) {
+            echo "Server of the URL has no HTTP/2.0 support.";
+        } else {
+            echo curl_error($ch);
+        }
+        curl_close($ch); */
+    }
+    
     private function hide_wordpress_generator(){
         remove_action("wp_head", "wp_generator");
     }
 }
-(new wphardener());
+
+add_action("init", function(){
+    (new wphardener());
+});
 ?>
